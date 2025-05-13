40a4658d2635e188a7d04c56e762da1028c8c37a
byEriK12 <eric.matasp12@gmail.com>
2025-05-13 20:10:40 +0200

    todo merge

---
+++ app.js
@@ -0,0 +1,49 @@
+  
+  // Detectar clic en "Inici"
+  document.getElementById("inicio").addEventListener("click", function(event) {
+    event.preventDefault(); // Evita que navegue inmediatamente
+    // Aquí puedes hacer cualquier acción antes de redirigir, por ejemplo:
+    // alert("Iràs a Inici");
+    alert("Aniràs a la pàgina d'inici (index.html)");
+    window.location.href = "index.html"; // Redirige manualmente
+    
+  });
+
+  // Detectar clic en "Models"
+  document.getElementById("models").addEventListener("click", function(event) {
+    event.preventDefault();
+    alert("Aniràs a la pàgina de models (models.html)");
+    window.location.href = "models.html";
+  });
+
+  // Detectar clic en "Recursos"
+  document.getElementById("resources").addEventListener("click", function(event) {
+    event.preventDefault();
+    alert("Aniràs a la pàgina de recursos (resources.html)");
+    window.location.href = "resources.html";
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const taula = document.getElementById("taulaComparativa");
+    const boto = document.getElementById("toggleTaula");
+
+    // Ocultar la tabla al inicio
+    taula.style.display = "none";
+
+    // Añadir listener al botón
+    boto.addEventListener("click", () => {
+      if (taula.style.display === "none") {
+        taula.style.display = "table";
+        boto.textContent = "Amagar taula";
+        alert("Has amagado la tabla")
+      } else {
+        taula.style.display = "none";
+        boto.textContent = "Mostrar taula";
+        alert("Has mostrat la tabla")
+      }
+    });
+  });
+
+function canviarTema() {
+  document.body.classList.toggle('tema-clar');
+}
